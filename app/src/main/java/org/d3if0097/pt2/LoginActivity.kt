b63e8b447f4293f63945b1a4678fac806eb6fb54
Fauzi Aulia <auliafauzi835@gmail,com>
2023-06-10 11:43:01 +0700

    Penyempurnaan Ui

--- app/src/main/java/org/d3if0097/pt2/LoginActivity.kt
+++ app/src/main/java/org/d3if0097/pt2/LoginActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat.startActivity
@@ -75,4 +76,5 @@ class LoginActivity : AppCompatActivity() {
 
             })
         }
+
     }
